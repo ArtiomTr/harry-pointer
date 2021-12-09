650fa3192492a3c26078a2b0225d94679b6a4f49
Edvinas <edvinas.igaris@gmail.com>
2021-12-09 18:04:19 +0200

    Program start

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
+#include "node.h"
+#include "utils.c"
+#include "utils.h"
 #include <stdio.h>
 
+
+HPNode node1 = {.name = "Blabla", .kind = 8};
 int main() {
-    printf("Hello world!\n");
+    HPChildren children = parseChildren(&node1);
 
     return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,1 +1,17 @@
 #include "utils.h"
+#include "node.h"
+#include "stdlib.h"
+
+HPChildren parseChildren(HPNode *node, ...) {
+    HPNode nodes[1000];
+    int count = 0;
+    int count = 10;
+    va_list nodeList;
+    va_start(nodeList, node);
+
+    for(HPNode *i = node; i != NULL; i = va_arg(nodeList, HPNode *)) {
+        nodes[count] = *i;
+        count++;
+    }
+    calloc(count, sizeof(HPNode));
+}
